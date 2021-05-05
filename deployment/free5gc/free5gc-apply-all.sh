e1b9201f42eea4a93a15ca8143911c005f923567
p76081158 <p76081158@gs.ncku.edu.tw>
2021-05-05 16:03:00 +0800

    add crd & subnet to free5gc deployment

--- deployment/free5gc/free5gc-apply-all.sh
+++ deployment/free5gc/free5gc-apply-all.sh
@@ -11,6 +11,8 @@ cd ../../
 
 kubectl apply -f ../namespace/free5gc.yaml
 kubectl apply -f ../network-attachment-definition/
+kubectl apply -f ../custom-resource-definition/
+kubectl apply -f ../subnet/
 cd mongodb      && kustomizeapply base
 #cd UERANSIM-gnb && kustomizeapply base
 #cd UERANSIM-ue  && kustomizeapply base
