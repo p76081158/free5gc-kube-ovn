2b1f4f276dd3102254cf81c1b91f31d1ece79729
p76081158 <p76081158@gs.ncku.edu.tw>
2021-05-28 16:22:42 +0800

    auto shell script of gnb

---
+++ deployment/simulator/gnb-clear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 466-01-000000010
+kubectl delete -k UERANSIM-gnb-466-01-000000010/base
+
+# 466-11-000000010
+kubectl delete -k UERANSIM-gnb-466-11-000000010/base
+
+# 466-93-000000010
+kubectl delete -k UERANSIM-gnb-466-93-000000010/base
+
+
