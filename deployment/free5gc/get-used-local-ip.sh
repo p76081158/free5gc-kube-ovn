0beb42f39ae5e92c7f0140fd922ccfe0bf61a8e7
p76081158 <p76081158@gs.ncku.edu.tw>
2021-06-09 19:07:46 +0800

    move dir

---
+++ deployment/free5gc/get-used-local-ip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Please enter CIDR!"
+    exit
+else
+    echo "List used ip in $1/24 !"
+fi
+
+# input example: local_ip="192.168.72."
+local_ip="$1"
+status=$(dpkg -s nmap | grep Status)
+
+if [ -z "$status" ]
+then
+    echo "Install nmap"
+    sudo apt isntall nmap
+fi
+
+sudo nmap -sP -PR $local_ip* | grep $local_ip
