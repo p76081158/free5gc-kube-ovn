d852d63d219e67eb77fc151db21f90b1bb4dbeaa
p76081158 <p76081158@gs.ncku.edu.tw>
2021-06-09 15:25:48 +0800

    move dir

--- deployment/free5gc/free5gc-clear-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl delete namespace free5gc
-#kubectl delete -f ../network-attachment-definition/
-kubectl delete -f ../custom-resource-definition/
-kubectl delete persistentvolume -l namespace=free5gc
