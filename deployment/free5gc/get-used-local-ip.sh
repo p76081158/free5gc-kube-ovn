889a6635d0cd24641904de3388e0b0e9b2350e87
Vcx <p76081158@gs.ncku.edu.tw>
2021-10-19 12:48:54 +0800

    fix install bug

--- deployment/free5gc/get-used-local-ip.sh
+++ deployment/free5gc/get-used-local-ip.sh
@@ -15,7 +15,7 @@ status=$(dpkg -s nmap | grep Status)
 if [ -z "$status" ]
 then
     echo "Install nmap"
-    sudo apt isntall nmap
+    sudo apt install nmap
 fi
 
 sudo nmap -sP -PR $local_ip* | grep $local_ip
